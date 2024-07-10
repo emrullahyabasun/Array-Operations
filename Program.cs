da6b7d20f5f041343e40c7e71d61b6d97207aa90
emrullahyabasun <yabasunemrullah@gmail.com>
2024-07-10 22:01:04 +0300

    pangram resolve

--- Program.cs
+++ Program.cs
@@ -4,36 +4,27 @@
     {
         static void Main(string[] args)
         {
-            string[] pallets = { "B14", "A11", "B12", "A13" };
-            Console.WriteLine("");
+            string pangram = "The quick brown fox jumps over the lazy dog";
+            //Write the code necessary to reverse the letters of each word in place and display the result.
+            string[] words = pangram.Split(' ');
 
-            Array.Clear(pallets, 0, 2);
-            Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
-            foreach (var pallet in pallets)
+            for (int i = 0; i < words.Length; i++)
             {
-                Console.WriteLine($"-- {pallet}");
+                char[] letters = words[i].ToCharArray();
+                int left = 0;
+                int right = letters.Length - 1;
+                while (left < right)
+                {
+                    char temp = letters[left];
+                    letters[left] = letters[right];
+                    letters[right] = temp;
+                    left++;
+                    right--;
+                }
+                words[i] = new string(letters);
             }
-
-            Console.WriteLine("");
-            Array.Resize(ref pallets, 6);
-            Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
-
-            pallets[4] = "C01";
-            pallets[5] = "C02";
-
-            foreach (var pallet in pallets)
-            {
-                Console.WriteLine($"-- {pallet}");
-            }
-
-            Console.WriteLine("");
-            Array.Resize(ref pallets, 3);
-            Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
-
-            foreach (var pallet in pallets)
-            {
-                Console.WriteLine($"-- {pallet}");
-            }
+            pangram = string.Join(" ", words);
+            System.Console.WriteLine(pangram);
         }
     }
 }
