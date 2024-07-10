569dcdb47ead10f69aadda2322b2a173b240ef37
emrullahyabasun <yabasunemrullah@gmail.com>
2024-07-10 21:42:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Array_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pallets = { "B14", "A11", "B12", "A13" };
+
+            Console.WriteLine("reverse Sorted...");
+            Array.Reverse(pallets);
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Array.Sort(pallets);
+            //print the sorted array on console
+            Console.WriteLine(string.Join(", ", pallets));
+
+           
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+        }
+    }
+}
