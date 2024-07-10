a7d5f0af19b728221e4bcc35c7c6aad7a2ad09bf
emrullahyabasun <yabasunemrullah@gmail.com>
2024-07-10 21:47:00 +0300

    Array Operators

--- Program.cs
+++ Program.cs
@@ -5,19 +5,13 @@
         static void Main(string[] args)
         {
             string[] pallets = { "B14", "A11", "B12", "A13" };
+            Console.WriteLine("");
 
-            Console.WriteLine("reverse Sorted...");
-            Array.Reverse(pallets);
-            foreach (var pallet in pallets)
-            {
-                Console.WriteLine($"-- {pallet}");
-            }
+            Console.WriteLine($"Before: {pallets[0]}");
+            Array.Clear(pallets, 0, 2);
+            Console.WriteLine($"After: {pallets[0]}");
 
-            Array.Sort(pallets);
-            //print the sorted array on console
-            Console.WriteLine(string.Join(", ", pallets));
-
-           
+            Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
             foreach (var pallet in pallets)
             {
                 Console.WriteLine($"-- {pallet}");
